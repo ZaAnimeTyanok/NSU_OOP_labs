375148b354b0c6a2176627717cb227261d350f4e
Yanchenko Sergey <144523679+ZaAnimeTyanok@users.noreply.github.com>
2024-10-22 18:32:45 +0700

    Add files via upload

---
+++ lab0_b/src/main.cpp
@@ -0,0 +1,24 @@
+﻿#include "word_counter.h"
+//#include "memchek.h"      // <- using for detect memory leaks in vs
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cout << "error: incorrect number of arguments" << std::endl;
+
+        return 0;
+
+    }
+
+    WordCounter* wordCounter = new WordCounter(argv[1], argv[2]);
+    
+    (*wordCounter).read_file();
+    (*wordCounter).write_to_file();
+    
+    delete wordCounter;
+    
+    //check_memory();       // <- using for detect memory leaks in vs
+    
+    return 0;
+
+}
+
